223032c08b17108f438bd5da98fdf5ea97603d33
Vincent Revard <Vincent.Revard@gmail.com>
2024-02-09 16:30:33 -0800

    add POST / Del and PATCH almost

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 //----------------------------- START OF CODE
 // index.js
-//! THE ONLY GENERAL NOTE IS:
-//* 1. YOU did AWESOME, very clear, clean, and logical
-//TODO 2. I would start paying attention to indentation or start using an auto-indent tool
 
-
-//! GREAT job with the globals - MATTEO
 const ramensURL = 'http://localhost:3000/ramens'
 const ramenMenu = document.querySelector('#ramen-menu')
 const ramenDetailImage = document.querySelector('.detail-image')
@@ -15,13 +10,13 @@ const ratingDisplay = document.querySelector('#rating-display')
 const commentDisplay = document.querySelector('#comment-display')
 const newRamenForm = document.querySelector('#new-ramen')
 const deleteRamenMenuItm = document.querySelector('#ramen-del-btn')
-
+const updateRamenMenuData = document.querySelector('#submit-update')
 
 // Callbacks
-//! PERFECT job with the handleClick callback, no notes! - MATTEO
+
 
 const handleClick = (ramen) => {
-  //! creating a function that will pull in the ramen data and fill out the details 
+    //! creating a function that will pull in the ramen data and fill out the details 
     ramenDetailImage.src = ramen.image
     ramenDetailImage.alt = ramen.name //* Good catch ading an alt - MATTEO
     ramenDetailName.textContent = ramen.name
@@ -30,8 +25,6 @@ const handleClick = (ramen) => {
     commentDisplay.textContent = ramen.comment
 }
 
-
-//! PERFECT job with the addSubmitListener callback, no notes! - MATTEO
 
 const addSubmitListener = (e) => {
     e.preventDefault()
@@ -46,12 +39,8 @@ const addSubmitListener = (e) => {
     debugger
     //*Trying POST
     postJSON(ramensURL, newRamen)
-    .then((createdRamen) => displayRamens(createdRamen))
-        // Create a new <p> element for the comment
-        // const newCommentParagraph = document.createElement('p')
-        // newCommentParagraph.textContent = createdRamen.comment
-        // commentDisplay.appendChild(newCommentParagraph)
-    .catch(console.error)
+        .then((createdRamen) => displayRamens(createdRamen))
+        .catch(console.error)
     debugger
     e.target.reset()
 
@@ -66,6 +55,8 @@ const addSubmitListener = (e) => {
 //* You used it to display ONE ramen, so the naming convention is a bit confusing - MATTEO
 //* Said that, there was a bit of confusion with the tests and which functions could not be removed - MATTEO
 //* So take it more as a general suggestion for the future - MATTEO
+
+
 const displayRamens = (ramensObj) => {
     const ramenMenuContainer = document.createElement('div')
     const ramenDelBtn = document.createElement('button')
@@ -85,32 +76,79 @@ const displayRamens = (ramensObj) => {
     ramenMenu.appendChild(ramenMenuContainer)
     ramenMenuContainer.appendChild(ramenDelBtn)
     ramenMenuContainer.appendChild(ramenImg)
+  //  const ramenID = ramensObj.id
 
-   // ramenDelBtn.querySelector('')
-   //! needing to re-select element with new assigned data-id attribute 
-const selectRamenDelId = document.querySelector(`div#ramen-menu > .ramen-item > button[data-id='${ramensObj.id}']`)
-const selectImageId = document.querySelector(`div#ramen-menu > .ramen-item > img[data-id='${ramensObj.id}']`)
-debugger
-const selectContainerID = document.querySelector(`div#ramen-menu > .ramen-item[data-id='${ramensObj.id}']`)
+    // ramenDelBtn.querySelector('')
+    //! needing to re-select element with new assigned data-id attribute 
+    const selectRamenDelId = document.querySelector(`div#ramen-menu > .ramen-item > button[data-id='${ramensObj.id}']`)
+    const selectImageId = document.querySelector(`div#ramen-menu > .ramen-item > img[data-id='${ramensObj.id}']`)
+    debugger
+    const selectContainerID = document.querySelector(`div#ramen-menu > .ramen-item[data-id='${ramensObj.id}']`)
 
- //   document.querySelectorAll('button[data-id]').forEach((button) => {
-    selectRamenDelId.addEventListener('click', function(event) {
+    //   document.querySelectorAll('button[data-id]').forEach((button) => {
+    selectRamenDelId.addEventListener('click', function (event) {
         let dataId = event.currentTarget.getAttribute('data-id')
         selectContainerID.remove()
         selectRamenDelId.remove() // remove deleted button from DOM via data-ID
         selectImageId.remove() // remove deleted image from DOM via data-ID
         deleteAnimal(`${ramensObj.id}`) //delete current object.id 
-        
-
     })
     debugger
+
+    
+    updateRamenMenuData.addEventListener('submit', updatedRamen)
+    const updatedRamen = (e) => {
+        e.preventDefault()
+        const updatedRamenRating = e.target.elements['update-rating'].value
+        const updatedRamenComment = e.target.elements['update-comment'].value
+        debugger
+        //*Trying PATCH
+        // patchJSON(updatedRamenRating)
+        // .then(res => res.json())
+        // .then((createdRamen) => displayRamens(createdRamen))
+        // .catch(console.error)
+        // debugger
+        // }
+        fetch(`http://localhost:3000/ramens/${ramenObj.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                rating: updatedRamenRating
+     //           comment: e.target.ramenObj.comment.value,
+            })
+        })
+    }
 }
 debugger
-    // Use append instead of appendChild , messes with things
+// Use append instead of appendChild , messes with things
 
+// const updatedRamen = (e) => {
+//     e.preventDefault()
+//     const updatedRamenRating = e.target.elements['update-rating'].value
+//     const updatedRamenComment = e.target.elements['update-comment'].value
+//     debugger
+//     //*Trying PATCH
+//     // patchJSON(updatedRamenRating)
+//     // .then(res => res.json())
+//     // .then((createdRamen) => displayRamens(createdRamen))
+//     // .catch(console.error)
+//     // debugger
+//     // }
+//     fetch(`http://localhost:3000/ramens/${ramenObj.id}`, {
+//         method: 'PATCH',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             rating: updatedRamenRating
+//  //           comment: e.target.ramenObj.comment.value,
+//         })
+//     })
+// }
 
-
-  //! PERFECT job with the getJSON utility function and even just including one!!! - MATTEO
+// updateRamenMenuData.addEventListener('submit', updatedRamen)
 
 const getJSON = (url) => {
     return fetch(url)
@@ -118,65 +156,54 @@ const getJSON = (url) => {
             if (resp.ok) {
                 return resp.json()
             } else {
-            throw resp.statusText
+                throw resp.statusText
             }
         })
 }
+
 const postJSON = (url, data) => {
     const configObj = {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
     }
     return fetch(url, configObj)
-        .then((resp) =>  {
+        .then((resp) => {
             if (resp.ok) {
                 return resp.json()
             } else {
                 throw resp.statusText
             }
         })
-    
 }
+
 
 const deleteAnimal = (id) => {
-    fetch(`http://localhost:3000/ramens/${id}`,{
+    fetch(`http://localhost:3000/ramens/${id}`, {
         method: 'DELETE',
         headers: {
-            'Content-Type':'application/json'
+            'Content-Type': 'application/json'
         }
     })
-    .then(res => {debugger})
-    //     res.json())
-    // .then(deletedRamen => console.log(deletedRamen))
+        .then(res => res.json())
+        .then(deletedRamen => console.log(deletedRamen))
 }
 
+
 const main = () => {
-// Invoke displayRamens here
-//! The following logic could have been encapsulated into a smaller function - MATTEO
-//! and then you could have invoked that function here - MATTEO
+    // Invoke displayRamens here
+    //! The following logic could have been encapsulated into a smaller function - MATTEO
+    //! and then you could have invoked that function here - MATTEO
     getJSON(ramensURL)
         .then((ramensData) => {
             handleClick(ramensData[0])
             ramensData.forEach((ramen) => displayRamens(ramen))
         })
         .catch(console.error)
-      // Invoke addSubmitListener here
+    // Invoke addSubmitListener here
     newRamenForm.addEventListener('submit', addSubmitListener)
 }
 
 main()
-
-
-// Export functions for testing
-//   export {
-//       displayRamens,
-//       addSubmitListener,
-//       handleClick,
-//       main,
-//   }
-
-
-//----------------------------- END OF CODE
